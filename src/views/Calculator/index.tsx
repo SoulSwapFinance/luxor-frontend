9ab54d77d3c3b029682dc0122dd9dbf9038b896a
Buns <BunsDev@users.noreply.github.com>
2021-12-02 18:43:54 -0600

    verbiage

--- src/views/Calculator/index.tsx
+++ src/views/Calculator/index.tsx
@@ -100,7 +100,7 @@ function Calculator() {
                                     </Grid>
                                     <Grid item xs={6} sm={4} md={4} lg={4}>
                                         <div className="calculator-card-index">
-                                            <p className="calculator-card-metrics-title">Your LUM Balance</p>
+                                            <p className="calculator-card-metrics-title">LUM Balance</p>
                                             <p className="calculator-card-metrics-value">{isAppLoading ? <Skeleton width="100px" /> : <>{trimmedLumensBalance} LUM</>}</p>
                                         </div>
                                     </Grid>
