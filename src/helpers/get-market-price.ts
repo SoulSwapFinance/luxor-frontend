e770d27bf3922353f1f83ffaec75a54f9b49f9e3
Buns <68980965+BunsDev@users.noreply.github.com>
2021-12-05 21:13:46 -0600

    fix: market price of LUX

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -1,12 +1,12 @@
 import { ethers } from "ethers";
 import { LpReserveContract } from "../abi";
-import { ftmLuxor } from "../helpers/bond";
+import { daiLuxor } from "../helpers/bond";
 import { Networks } from "../constants/blockchain";
 
 export async function getMarketPrice(networkID: Networks, provider: ethers.Signer | ethers.providers.Provider): Promise<number> {
-    const ftmLuxorAddress = ftmLuxor.getAddressForReserve(networkID);
-    const pairContract = new ethers.Contract(ftmLuxorAddress, LpReserveContract, provider);
+    const daiLuxorAddress = daiLuxor.getAddressForReserve(networkID);
+    const pairContract = new ethers.Contract(daiLuxorAddress, LpReserveContract, provider);
     const reserves = await pairContract.getReserves();
-    const marketPrice = reserves[0] / reserves[1];
+    const marketPrice = reserves[1] / reserves[0];
     return marketPrice;
 }
