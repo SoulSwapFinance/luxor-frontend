7487d5d150c7d528cc932d61a5a43182f28d3d8c
Buns <bunsthedev@gmail.com>
2021-12-07 23:22:58 -0600

    dashboard add: wLUM Price and Percent Staked

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Grid, Zoom } from "@material-ui/core";
-import { trim } from "../../helpers";
+import { formatCurrency, trim } from "../../helpers";
 import "./dashboard.scss";
 import { Skeleton } from "@material-ui/lab";
 import { IReduxState } from "../../store/slices/state.interface";
@@ -10,7 +10,11 @@ function Dashboard() {
     const isAppLoading = useSelector<IReduxState, boolean>(state => state.app.loading);
     const app = useSelector<IReduxState, IAppSlice>(state => state.app);
 
-    const trimmedStakingAPY = trim(app.stakingAPY * 100, 1);
+    const trimmedStakingAPY = trim(app.stakingAPY * 100, 0);
+    const percStaked = (app.circSupply / app.totalSupply) * 100;
+    const wLUMPrice = useSelector(state => {
+        return app.marketPrice * Number(app.currentIndex);
+    });
 
     return (
         <div className="dashboard-view">
@@ -37,6 +41,24 @@ function Dashboard() {
 
                         <Grid item lg={6} md={6} sm={6} xs={12}>
                             <div className="dashboard-card">
+                                <p className="card-title">wLUM Price</p>
+                                <p className="card-value">
+                                    {isAppLoading ? (
+                                        <Skeleton width="160px" />
+                                    ) : (
+                                        new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: "USD",
+                                            maximumFractionDigits: 0,
+                                            minimumFractionDigits: 0,
+                                        }).format(wLUMPrice)
+                                    )}
+                                </p>
+                            </div>
+                        </Grid>
+
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
+                            <div className="dashboard-card">
                                 <p className="card-title">Market Cap</p>
                                 <p className="card-value">
                                     {isAppLoading ? (
@@ -53,26 +75,23 @@ function Dashboard() {
                             </div>
                         </Grid>
 
-                        {/* <Grid item lg={6} md={6} sm={6} xs={12}>
+                        <Grid item lg={6} md={6} sm={6} xs={12}>
                             <div className="dashboard-card">
-                                <p className="card-title">Supply (Staked/Total)</p>
+                                <p className="card-title">Percent Staked</p>
                                 <p className="card-value">
                                     {isAppLoading ? (
                                         <Skeleton width="250px" />
                                     ) : (
-                                        `${new Intl.NumberFormat("en-US", {
-                                            maximumFractionDigits: 0,
-                                            minimumFractionDigits: 0,
-                                        }).format(app.circSupply)}
-                                        /
-                                        ${new Intl.NumberFormat("en-US", {
-                                            maximumFractionDigits: 0,
-                                            minimumFractionDigits: 0,
-                                        }).format(app.totalSupply)}`
+                                        `${
+                                            new Intl.NumberFormat("en-US", {
+                                                maximumFractionDigits: 2,
+                                                minimumFractionDigits: 0,
+                                            }).format(percStaked) + "%"
+                                        }`
                                     )}
                                 </p>
                             </div>
-                        </Grid> */}
+                        </Grid>
 
                         <Grid item lg={6} md={6} sm={6} xs={12}>
                             <div className="dashboard-card">
