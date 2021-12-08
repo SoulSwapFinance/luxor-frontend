4f8966ce7e441389a2005b65691cf9b4863950d3
Buns <bunsthedev@gmail.com>
2021-12-07 21:25:43 -0600

    update: ABIs, Mint LUX icon

--- src/helpers/bond/index.ts
+++ src/helpers/bond/index.ts
@@ -3,11 +3,13 @@ import { CustomLPBond, LPBond } from "./lp-bond";
 import { CustomBond, StableBond } from "./stable-bond";
 
 import DaiIcon from "../../assets/tokens/DAI.svg";
+import LuxIcon from "../../assets/tokens/LUXOR.svg";
 import FtmIcon from "../../assets/tokens/FTM.svg";
+// import SoulIcon from "../../assets/tokens/SOUL.png";
 import DaiLuxorIcon from "../../assets/tokens/DAI.svg";
 import FtmLuxorIcon from "../../assets/tokens/FTM.svg";
 
-import { StableBondContract, LpBondContract, WftmBondContract, StableReserveContract, LpReserveContract } from "../../abi";
+import { StableBondContract, DaiLpBondContract, FtmLpBondContract, WftmBondContract, StableReserveContract, LpReserveContract } from "../../abi";
 
 export const dai = new StableBond({
     name: "dai",
@@ -53,8 +55,8 @@ export const daiLuxor = new LPBond({
     name: "dai_lux_lp",
     displayName: "LUX-DAI LP",
     bondToken: "DAI",
-    bondIconSvg: DaiLuxorIcon,
-    bondContractABI: LpBondContract,
+    bondIconSvg: LuxIcon,
+    bondContractABI: DaiLpBondContract,
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
         [Networks.FTM]: {
@@ -69,12 +71,32 @@ export const daiLuxor = new LPBond({
     lpUrl: "https://app.soulswap.finance/exchange/add/0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E/0x6671E20b83Ba463F270c8c75dAe57e3Cc246cB2b",
 });
 
+// export const soulLuxor = new CustomLPBond({
+//     name: "soul_lux_lp",
+//     displayName: "LUX-SOUL LP",
+//     bondToken: "SOUL",
+//     bondIconSvg: LuxIcon,
+//     bondContractABI: LpBondContract,
+//     reserveContractAbi: LpReserveContract,
+//     networkAddrs: {
+//         [Networks.FTM]: {
+//             bondAddress: "0xfd4714Cf2CBB82F6013F44bB9218588a67716cd2",
+//             reserveAddress: "0x475a63154C3e85ac0F2CB453f0b5c63e4370333c",
+//         },
+//         [Networks.BSC]: {
+//             bondAddress: "",
+//             reserveAddress: "",
+//         },
+//     },
+//     lpUrl: "https://app.soulswap.finance/add/0x6671e20b83ba463f270c8c75dae57e3cc246cb2b/0xe2fb177009ff39f52c0134e8007fa0e4baacbd07",
+// });
+
 export const ftmLuxor = new CustomLPBond({
     name: "ftm_lux_lp",
     displayName: "LUX-FTM LP",
     bondToken: "FTM",
     bondIconSvg: FtmLuxorIcon,
-    bondContractABI: LpBondContract,
+    bondContractABI: FtmLpBondContract,
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
         [Networks.FTM]: {
