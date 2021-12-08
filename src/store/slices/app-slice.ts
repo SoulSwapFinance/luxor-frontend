b9c2ca778664456d983629c50db2672bbed084ef
Buns <bunsthedev@gmail.com>
2021-12-08 14:21:57 -0600

    fix: Index

--- src/store/slices/app-slice.ts
+++ src/store/slices/app-slice.ts
@@ -66,7 +66,7 @@ export const loadAppDetails = createAsyncThunk(
         const runway = Math.log(treasuryRunway) / Math.log(1 + stakingRebase) / 3;
 
         return {
-            currentIndex: Number(ethers.utils.formatUnits(currentIndex, "gwei")) / 4.5,
+            currentIndex: Number(ethers.utils.formatUnits(currentIndex, "gwei")), // / 4.5,
             totalSupply,
             marketCap,
             currentBlock,
