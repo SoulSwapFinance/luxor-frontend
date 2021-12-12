e4553b403d53844e2a67071fa7a9e772f28303b6
Buns <BunsDev@users.noreply.github.com>
2021-12-12 03:12:53 -0600

    resolve2

--- src/views/Bond/BondPurchase.tsx
+++ src/views/Bond/BondPurchase.tsx
@@ -120,7 +120,7 @@ function BondPurchase({ bond, slippage }: IBondPurchaseProps) {
         setZapinOpen(false);
     };
 
-    const displayUnits = useWFTM ? "FTM" : bond.displayUnits;
+    const displayUnits = useWFTM ? "WFTM" : bond.displayUnits;
 
     return (
         <Box display="flex" flexDirection="column">
@@ -129,7 +129,7 @@ function BondPurchase({ bond, slippage }: IBondPurchaseProps) {
                     <FormControl className="lux-input" variant="outlined" color="primary" fullWidth>
                         <div className="ftm-checkbox">
                             <input type="checkbox" checked={!useWFTM} onClick={() => setUseWFTM(!useWFTM)} />
-                            <p>Use FTM</p>
+                            <p>Use WFTM</p>
                         </div>
                     </FormControl>
                 )}
