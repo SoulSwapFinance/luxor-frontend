f6dff8aabca75326110ad8a9ab15e5f09a118ccb
Buns <BunsDev@users.noreply.github.com>
2021-12-12 03:09:48 -0600

    reselve

--- src/views/Bond/BondPurchase.tsx
+++ src/views/Bond/BondPurchase.tsx
@@ -128,7 +128,7 @@ function BondPurchase({ bond, slippage }: IBondPurchaseProps) {
                 {bond.name === "wftm" && (
                     <FormControl className="lux-input" variant="outlined" color="primary" fullWidth>
                         <div className="ftm-checkbox">
-                            <input type="checkbox" checked={useWFTM} onClick={() => setUseWFTM(!useWFTM)} />
+                            <input type="checkbox" checked={!useWFTM} onClick={() => setUseWFTM(!useWFTM)} />
                             <p>Use FTM</p>
                         </div>
                     </FormControl>
